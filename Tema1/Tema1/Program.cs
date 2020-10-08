0ac00f02b11f88e94c0c56ed25b1a910cba8baf1
Cristutiu Emanuel <cristutiumanu@gmail.com>
2020-10-08 16:40:46 +0300

    Solutia problema Moore's Law

---
+++ Tema1/Tema1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Tema1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Insert a value");
+
+            int n = int.Parse(Console.ReadLine());
+            double years = 1.5 * Math.Log(n, 2);
+      
+             Console.WriteLine(years);
+
+
+
+
+
+        }
+    }
+}
